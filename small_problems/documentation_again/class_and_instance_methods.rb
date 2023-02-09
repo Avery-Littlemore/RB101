b10f464b42e4bdf0fc93eb016bd3e3576129770f
Avery Littlemore <avery.d.littlemore@gmail.com>
2023-02-09 11:57:05 -0500

    Late Feb 8 dump

---
+++ small_problems/documentation_again/class_and_instance_methods.rb
@@ -0,0 +1,5 @@
+# Locate the ruby documentation for methods File::path and File#path. How are they different?
+
+# File::path is a class method, whereas File#path is an instance method.
+# Class methods are called on class
+# Instance methods are called on objects
---
+++ small_problems/documentation_again/optional_arguments_redux.rb
@@ -0,0 +1,8 @@
+require 'date'
+
+puts Date.civil
+puts Date.civil(2016)
+puts Date.civil(2016, 5)
+puts Date.civil(2016, 5, 13)
+
+# Come back to this one... haven't seen this yet.
